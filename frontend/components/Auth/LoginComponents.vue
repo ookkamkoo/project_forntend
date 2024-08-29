7395fed71665acff77c22e6d45d05a879aa9cd57
ookkamkoo <ookkamkoo@hotmail.com>
2024-08-29 23:01:51 +0700

    upload project

--- frontend/components/Auth/LoginComponents.vue
+++ frontend/components/Auth/LoginComponents.vue
@@ -1,16 +1,5 @@
+
 <template>
-   <h3 @click="ssss()">asdasdasd</h3>
-</template>
-<script setup lang="ts">
-  const config = useRuntimeConfig()
-  const url = config.public.serviceUrls;
-  console.log(url);
-    
-  const ssss = () =>{
-    console.log("aaaaaaaaaaaa"); 
-  }
-</script>
-<!-- <template>
   <a-flex gap="middle" vertical>
     <div className="login-container">
       <div class="form-login">
@@ -116,11 +105,7 @@
   import { login,setPinUser } from '~/services/authService';
   import { setToken,setName,setUsername,setPermission } from '~/auth/authToken';
   import { Alert } from '../Alert/alertComponent';
-  // import { connectWebSocket, sendMessage } from '~/services/socketService';
   
-  // import { connectWebSocket } from '~/services/socketService';
-  // emitEvent('aa', 'logn');
-  // connectWebSocket();
   const open = ref<boolean>(false);
   const openPin = ref<boolean>(false);
   const text = ref('');
@@ -209,7 +194,7 @@
     return !(formState.username && formState.password);
   });
 
-</>
+</script>
 
   <style>
   .login-container {
@@ -316,4 +301,4 @@
       padding-bottom: 1rem;
     }
   }
-  </style> -->
+  </style>
