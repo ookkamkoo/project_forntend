e291cd8c94e5985918b10821ba000ada54a8767a
ookkamkoo <ookkamkoo@hotmail.com>
2024-08-29 18:07:52 +0700

    upload project

--- frontend/middleware/auth.ts
+++ frontend/middleware/auth.ts
@@ -10,16 +10,21 @@ export default defineNuxtRouteMiddleware(async (to, from) => {
     try {
         console.log(to.path);
         if (to.path !== '/') {
+            console.log("aaaaaaaaaaaaaa");
+            
             if (token) {
+                console.log("a1");
                 const check = await checkToken(token);
                 if(!check){
                     logout()
                     return navigateTo('/');
                 }
             } else {
+                console.log("a2");
                 return navigateTo('/');
             }
         } else {
+            console.log("bbbbbbbbbbbbbbbbb");
             if (token) {
                 const check = await checkToken(token);
                 console.log(check);
