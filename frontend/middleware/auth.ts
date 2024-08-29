4b0f1978942ac9cf2db927a649eefed5c002512e
ookkamkoo <ookkamkoo@hotmail.com>
2024-08-29 19:05:32 +0700

    upload project

--- frontend/middleware/auth.ts
+++ frontend/middleware/auth.ts
@@ -7,43 +7,33 @@ export default defineNuxtRouteMiddleware(async (to, from) => {
     if (typeof localStorage !== 'undefined') {
         token = getToken();
     }
-    // try {
-    //     console.log(to.path);
-    //     if (to.path !== '/') {
-    //         console.log("aaaaaaaaaaaaaa");
-            
-    //         if (token) {
-    //             console.log("a1");
-    //             const check = await checkToken(token);
-    //             if(!check){
-    //                 logout()
-    //                 return navigateTo('/');
-    //             }
-    //         } else {
-    //             console.log("a2");
-    //             return navigateTo('/');
-    //         }
-    //     } else {
-    //         console.log("bbbbbbbbbbbbbbbbb");
-    //         if (token) {
-    //             const check = await checkToken(token);
-    //             console.log(check);
+    try {
+        if (to.path !== '/') {
+            if (token) {
+                const check = await checkToken(token);
+                if(!check){
+                    logout()
+                    return navigateTo('/');
+                }
+            } else {
+                return navigateTo('/');
+            }
+        } else {
+            if (token) {
+                const check = await checkToken(token);
+                console.log(check);
                 
-    //             if(check){
-    //                 // return navigateTo('/deposit-withdraw/credit-custom');
-    //                 // return navigateTo('/report/game');
-    //             }else{
-    //                 Alert("error","กรุณาเข้าสู่ระบบใหม่อีกครั้ง ใหม่อีกครั้ง.")
-    //                 // logout();
-    //             }
-    //         }
-    //     }
-    // } catch (error) {
-    //     console.log("222222222222222222");
-        
-    //     // logout();
-    //     console.log(error);
-        
-    //     // return navigateTo('/');
-    // }
+                if(check){
+                    // return navigateTo('/deposit-withdraw/credit-custom');
+                    return navigateTo('/report/game');
+                }else{
+                    Alert("error","กรุณาเข้าสู่ระบบใหม่อีกครั้ง ใหม่อีกครั้ง.")
+                    // logout();
+                }
+            }
+        }
+    } catch (error) {
+        logout();
+        return navigateTo('/');
+    }
 });
