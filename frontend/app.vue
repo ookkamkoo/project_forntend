b86590af88faf52dcfe6656d4b1b15c6bf0aef07
ookkamkoo <ookkamkoo@hotmail.com>
2024-09-01 03:21:06 +0700

    upload project

--- frontend/app.vue
+++ frontend/app.vue
@@ -44,24 +44,6 @@ html, body {
   margin: 0 !important;
 }
 
-.loader-overlay {
-  position: fixed;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background-color: rgba(255, 255, 255, 0.8);
-  z-index: 9999;
-}
-
-.custom-spinner {
-  font-size: 24px;
-  animation: spin 2s linear infinite;
-}
-
 @keyframes spin {
   0% { transform: rotate(0deg); }
   100% { transform: rotate(360deg); }
